84323f08bc2cb0a47a395d9aa8f0cda32eec6344
Ashutosh <ashutoshorisoh@gmail.com>
2024-12-13 20:35:13 +0530

    landing page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
-import Home from './Home'
+import React from 'react'
+import Home from './Home.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const navigate = useNavigate(); // Hook for programmatic navigation
-
-  // Example function to navigate to '/home'
-  const goToHome = () => {
-    navigate('/home');
-  }
-
   return (
-    <div>
-      {/* Button for navigation */}
-      <button onClick={goToHome}>Go to Home</button>
-
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<div>Welcome to the App! <button onClick={goToHome}>Go to Home</button></div>} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </div>
+    <div><Home></Home></div>
   )
 }
 
-export default function AppWithRouter() {
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-}
+export default App
